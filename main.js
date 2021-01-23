ea2eac3e3844bb284114a3385d789d2259ade1d9
BeforeLast <Bfr.Last@Gmail.com>
2021-01-23 12:47:31 +0700

    SnL v1.1 release

--- main.js
+++ main.js
@@ -13,16 +13,23 @@ var textjokes = ["Why do programmers always mix up Christmas and Halloween?\nBec
                 ]
 var imagejokes = fs.readdirSync('./jokes');
 var i;
+var friendlist = [];
 
 // Status Check
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// Function
+function isFriend(id){
+    return friendlist.includes(id);
+}
+
 // Commands
 client.on('message', msg => {
     if (msg.channel.type == 'dm'){
         if (msg.content.toLowerCase() === 'yes') {
+            friendlist.push(msg.author.id);
             msg.author
                 .send('YAAY!! :laughing:\nWe are now officially a friend!\nIt is a friend duty to help each other, right?\n')
                 .then(console.log)
@@ -33,42 +40,78 @@ client.on('message', msg => {
                 .then(console.log)
                 .catch(console.error)
         } else if (msg.content.toLowerCase() === 'help') {
+            if (isFriend(msg.author.id)) {
                 msg.author
                     .send("What do you want?\n1. Give me something funny\n2. Ban someone in discord server\n3. About\n4. Bye")
                     .then(console.log)
                     .catch(console.error)
-        } else if (msg.content.toLowerCase() === 'give me something funny') {
-            if ((Math.floor(Math.random()*1000) % 2) == 0){
-                i = Math.floor(Math.random()*1000) % imagejokes.length;
-                msg.author
-                .send({files:[`./jokes/${imagejokes[i]}`]})
-                .then(console.log)
-                .catch(console.error)
             } else {
-                i = Math.floor(Math.random()*1000) % textjokes.length;
                 msg.author
-                .send(textjokes[i])
-                .then(console.log)
-                .catch(console.error)
+                    .send("Leave me alone!")
+                    .then(console.log)
+                    .catch(console.error)
+            }
+        } else if (msg.content.toLowerCase() === 'give me something funny') {
+            if (isFriend(msg.author.id)) {
+                if ((Math.floor(Math.random()*1000) % 2) == 0){
+                    i = Math.floor(Math.random()*1000) % imagejokes.length;
+                    msg.author
+                    .send({files:[`./jokes/${imagejokes[i]}`]})
+                    .then(console.log)
+                    .catch(console.error)
+                } else {
+                    i = Math.floor(Math.random()*1000) % textjokes.length;
+                    msg.author
+                    .send(textjokes[i])
+                    .then(console.log)
+                    .catch(console.error)
+                }
+            } else {
+                msg.author
+                    .send("Leave me alone!")
+                    .then(console.log)
+                    .catch(console.error)
             }
         } else if (msg.content.toLowerCase() === 'ban someone in discord server') {
-            msg.author
-                .send("I'm sorry but I cannot do that.. :cry:")
-                .then(console.log)
-                .catch(console.error)
+            if (isFriend(msg.author.id)) {
+                msg.author
+                    .send("I'm sorry but I cannot do that.. :cry:")
+                    .then(console.log)
+                    .catch(console.error)
+            } else {
+                msg.author
+                    .send("Leave me alone!")
+                    .then(console.log)
+                    .catch(console.error)
+            }
         } else if (msg.content.toLowerCase() === 'about') {
-            msg.author
-                .send("What? You want to know about my mean creator?\nSure, have a look : https://github.com/BeforeLast/SadNLonelyBot")
-                .then(console.log)
-                .catch(console.error)
+            if (isFriend(msg.author.id)) {
+                msg.author
+                    .send("What? You want to know about my mean creator?\nSure, have a look : https://github.com/BeforeLast/SadNLonelyBot")
+                    .then(console.log)
+                    .catch(console.error)
+            } else {
+                msg.author
+                    .send("Leave me alone!")
+                    .then(console.log)
+                    .catch(console.error)
+            }
         } else if (msg.content.toLowerCase() === 'bye') {
-            msg.author
-                .send("WHAT!? You're leaving me..? :cry:\nOkay then.. Goodbye.. :sob:")
-                .then(console.log)
-                .catch(console.error)
+            if (isFriend(msg.author.id)) {
+                msg.author
+                    .send("WHAT!? You're leaving me..? :cry:\nOkay then.. Goodbye.. :sob:")
+                    .then(console.log)
+                    .catch(console.error)
+            } else {
+                msg.author
+                    .send("Leave me alone!")
+                    .then(console.log)
+                    .catch(console.error)
+            }
+            
         } else {
             msg.author
-                .send('Hi! Thanks for talking to me. It\'s been a long time since someone talked to me.. :sob:\nI was made not long ago but I was abandoned after a few changes..\nMy creator is a really mean person.. :cry:\n Do you want to be my friend?\n1. Yes\n2. No')
+                .send('Hi! Thanks for talking to me. It\'s been a long time since someone talked to me.. :sob:\nI was made not long ago but I was abandoned after a few changes..\nMy creator is a really mean person.. :cry:\n Recently I have been updated to version 1.1! But no one congratulates me.. :cry:\n Do you want to be my friend?\n1. Yes\n2. No')
                 .then(console.log)
                 .catch(console.error);
         }
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
-SAD AND LONELY BOT v1.0
+SAD AND LONELY BOT v1.1
 
 Hello, this is sad and lonely bot. A simple discord robot just to waste your time.
+
+Change :
+-Added "friendlist" for bot to check whether someone is in "friendlist" or not
+-Certain command can only be used if the person dm the bot is on the bot "friendlist"
